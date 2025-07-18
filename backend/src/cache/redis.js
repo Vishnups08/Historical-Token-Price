c951c808c50d7f15aff1d13f9f25101a8e9b2832
Vishnu PS <119946293+Vishnups08@users.noreply.github.com>
2025-07-18 23:04:45 +0530

    Update redis.js

--- backend/src/cache/redis.js
+++ backend/src/cache/redis.js
@@ -3,6 +3,7 @@ const Redis = require('ioredis');
 const redis = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: 1,
   lazyConnect: true,
+  keepAlive: 10000,
 });
 
 redis.on('connect', () => {
